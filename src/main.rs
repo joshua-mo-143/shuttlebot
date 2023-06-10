d9d932f7ad4be6d1bc494a92f7f32a796c1ca096
Joshua Mo <joshua.mo.876@gmail.com>
2023-06-10 10:21:44 +0100

    refactor: remake bot with poise

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,79 @@
 use octocrab::Octocrab;
-use serenity::prelude::*;
+use poise::serenity_prelude as serenity;
+use shuttle_poise::ShuttlePoise;
 use shuttle_secrets::SecretStore;
 use sqlx::PgPool;
-
 mod bot;
 mod commands;
 mod utils;
 
 use bot::Bot;
+use commands::DocsLinks;
 use utils::get_secrets;
 
+pub struct Data {
+    pool: PgPool,
+    crab: Octocrab,
+}
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Responds with "world!"
+#[poise::command(slash_command)]
+async fn hello(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("world!").await?;
+    Ok(())
+}
+
+/// Link to Shuttle documentation
+#[poise::command(slash_command)]
+pub async fn docs(
+    ctx: Context<'_>,
+    #[description = "The docs you want to link to"] docs: DocsLinks,
+) -> Result<(), Error> {
+    ctx.say(docs.to_link()).await?;
+    Ok(())
+}
+
 #[shuttle_runtime::main]
-async fn serenity(
+async fn poise(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
     #[shuttle_shared_db::Postgres] pool: PgPool,
-) -> shuttle_serenity::ShuttleSerenity {
+) -> ShuttlePoise<Data, Error> {
     // Get the discord token set in `Secrets.toml`
     let (discord_token, github_token) = get_secrets(secret_store).unwrap();
 
-    let octocrab = Octocrab::builder()
+    let pool2 = pool.clone();
+
+    let crab = Octocrab::builder()
         .personal_token(github_token)
         .build()
         .expect("Failed to build Octocrab instance");
 
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents =
-        GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-
-    let client = Client::builder(&discord_token, intents)
-        .event_handler(Bot { pool, octocrab })
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![hello(), docs()],
+            ..Default::default()
+        })
+        .client_settings(|f| {
+            f.intents(
+                serenity::GatewayIntents::GUILDS
+                    | serenity::GatewayIntents::GUILD_MESSAGES
+                    | serenity::GatewayIntents::MESSAGE_CONTENT,
+            )
+            .event_handler(Bot { pool: pool2 })
+        })
+        .token(discord_token)
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data { pool, crab })
+            })
+        })
+        .build()
         .await
-        .expect("Err creating client");
+        .map_err(shuttle_runtime::CustomError::new)?;
 
-    Ok(client.into())
+    Ok(framework.into())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,17 @@
+use anyhow::anyhow;
 use shuttle_secrets::SecretStore;
 
 pub fn get_secrets(secrets: SecretStore) -> Result<(String, String), anyhow::Error> {
     let discord_token = if let Some(discord_token) = secrets.get("DISCORD_TOKEN") {
         discord_token
     } else {
-        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
+        return Err(anyhow!("'DISCORD_TOKEN' was not found"));
     };
 
     let github_token = if let Some(github_token) = secrets.get("GITHUB_TOKEN") {
         github_token
     } else {
-        return Err(anyhow!("'GITHUB_TOKEN' was not found").into());
+        return Err(anyhow!("'GITHUB_TOKEN' was not found"));
     };
 
     Ok((discord_token, github_token))
