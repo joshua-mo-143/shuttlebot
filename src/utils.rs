32a9bc6261007d663e7939ab09bef3f1445023e4
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2023-06-12 00:04:53 +0100

    feat: migrations, add sql statements for db management (#3)

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,8 @@
+use crate::{Context, Error};
 use anyhow::anyhow;
+use poise::serenity_prelude::model::channel::GuildChannel;
 use shuttle_secrets::SecretStore;
+use tracing::error;
 
 pub fn get_secrets(secrets: SecretStore) -> Result<(String, String), anyhow::Error> {
     let discord_token = if let Some(discord_token) = secrets.get("DISCORD_TOKEN") {
@@ -15,4 +18,31 @@ pub fn get_secrets(secrets: SecretStore) -> Result<(String, String), anyhow::Err
     };
 
     Ok((discord_token, github_token))
+}
+
+pub struct Thread {}
+
+impl Thread {
+    pub async fn get(ctx: Context<'_>) -> GuildChannel {
+        // unwrap should be fine here as channel/guild ID should always be present in the happy path
+        ctx.serenity_context()
+            .http
+            .get_channel(ctx.channel_id().into())
+            .await
+            .unwrap()
+            .guild()
+            .unwrap()
+    }
+
+    pub async fn set_locked_status(ctx: Context<'_>, status: bool) -> Result<(), Error> {
+        if let Err(e) = Thread::get(ctx)
+            .await
+            .edit_thread(ctx.serenity_context().http.clone(), |f| f.locked(status))
+            .await
+        {
+            error!("Couldn't lock thread: {:?}", e);
+        }
+
+        Ok(())
+    }
 }
