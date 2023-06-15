3c2b31864144486834be3e54edc2e4895c2fcbfb
Joshua Mo <joshua.mo.876@gmail.com>
2023-06-15 19:27:45 +0100

    refactor: directory

---
+++ backend/migrations/20230609121212_init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS issues (
+    Id SERIAL PRIMARY KEY,
+    DiscordThreadId VARCHAR UNIQUE NOT NULL,
+    Origin VARCHAR NOT NULL DEFAULT 'discord',
+    SevCat SMALLINT NOT NULL CHECK (SevCat >= 1 AND SevCat <= 5),
+    InitialMessage VARCHAR,
+    FirstResponseUser VARCHAR,
+    ResolverUserId VARCHAR,
+    GithubLink VARCHAR,
+    Locked BOOLEAN NOT NULL DEFAULT false,
+    LockStatusChangeReason VARCHAR,
+    Resolved BOOLEAN NOT NULL DEFAULT false,
+    Created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    LastUpdated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FirstResponseTimedate TIMESTAMP WITH TIME ZONE,
+    ResolvedTimedate TIMESTAMP WITH TIME ZONE
+);
---
+++ backend/src/main.rs
@@ -0,0 +1,70 @@
+use octocrab::Octocrab;
+use shuttle_secrets::SecretStore;
+use sqlx::PgPool;
+use std::path::PathBuf;
+mod bot;
+mod commands;
+mod router;
+mod utils;
+
+use bot::init_discord_bot;
+use router::init_router;
+use utils::get_secrets;
+
+pub struct Data {
+    pool: PgPool,
+    crab: Octocrab,
+}
+
+struct CustomService {
+    pool: PgPool,
+    bot: Bot,
+    public: PathBuf
+}
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+type Bot = poise::FrameworkBuilder<
+    Data,
+    Box<(dyn std::error::Error + std::marker::Send + Sync + 'static)>,
+>;
+
+#[shuttle_runtime::main]
+async fn custom(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_static_folder::StaticFolder(folder = "templates")] public: PathBuf ) -> Result<CustomService, shuttle_runtime::Error> {
+
+    sqlx::migrate!().run(&pool).await.expect("Found an error while running migrations");
+
+    // Get the discord token set in `Secrets.toml`
+    let (discord_token, github_token) = get_secrets(secret_store).unwrap();
+
+    let crab = Octocrab::builder()
+        .personal_token(github_token)
+        .build()
+        .expect("Failed to build Octocrab instance");
+
+    let bot = init_discord_bot(discord_token, pool.clone(), crab)
+        .await
+        .unwrap();
+
+    Ok(CustomService { pool, bot, public })
+}
+
+#[shuttle_runtime::async_trait]
+impl shuttle_runtime::Service for CustomService {
+    async fn bind(mut self, addr: std::net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
+        let router = init_router(self.public);
+
+        let serve_router = axum::Server::bind(&addr).serve(router.into_make_service());
+
+        tokio::select! {
+            _ = self.bot.run() => {},
+            _ = serve_router => {}
+        };
+
+        Ok(())
+    }
+}
